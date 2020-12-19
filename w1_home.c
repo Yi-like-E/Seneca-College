9f597fa481ae420257ef6196d902fb7d228df461
yihsun-lee <62215204+yihsun-lee@users.noreply.github.com>
2020-12-19 13:48:14 -0500

    Delete w1_home.c

--- w1_home.c
+++
@@ -1,9 +0,0 @@
-/*Name:YiHsun Lee  Student ID.:129713202 Email:ylee212@myseneca.ca*/
-
-#include <stdio.h>
-int main(void)
-{
-	printf("********************************\n*** Welcome to C Programming ***\n********************************\n");
-
-	return 0;
-}
