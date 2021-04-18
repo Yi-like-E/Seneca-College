433f372c24c822dcb868cab99b1ac5dd2836866b
Yi <62215204+Yi-like-E@users.noreply.github.com>
2021-04-18 12:50:42 -0400

    Add files via upload

---
+++ Semester 2/OOP244/WS6 Lab/TextFile.h
@@ -0,0 +1,45 @@
+#ifndef SDDS_TEXTFILE_H__
+#define SDDS_TEXTFILE_H__
+#include <iostream>
+namespace sdds {
+class Text;
+class Line {
+    char* m_value ;
+    operator const char* ()const;
+    Line();
+    Line& operator=(const char*);
+    ~Line();
+    friend class TextFile;
+    // copy and copy assignment prevention goes here
+    Line& operator=(const Line&) = delete;
+    Line(const Line&) = delete;
+};
+
+class TextFile {
+    Line* m_textLines = nullptr;
+    char* m_filename = nullptr;
+    unsigned m_noOfLines; // an unsigned int to be set to the number of lines in the file
+    unsigned m_pageSize; // number of lines that should be displayed
+    void setFilename(const char* fname, bool isCopy = false);
+    void setNoOfLines();
+    void loadText();
+    void saveAs(const char *fileName)const;
+    void setEmpty();
+public:
+    TextFile(unsigned pageSize = 15);
+    TextFile(const char* filename, unsigned pageSize = 15);
+    TextFile(const TextFile&);
+    TextFile& operator=(const TextFile&);
+    ~TextFile();
+    std::ostream& view(std::ostream& ostr)const;
+    std::istream& getFile(std::istream& istr);
+    operator bool()const;
+    unsigned lines()const;
+    const char* name()const;
+    const char* operator[](unsigned index)const;
+};
+std::ostream& operator<<(std::ostream& ostr, const TextFile& text);
+std::istream& operator>>(std::istream& istr, TextFile& text);
+}
+
+#endif // !SDDS_TEXTFILE_H__
