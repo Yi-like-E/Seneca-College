f38cead64e1a0edf5d8d45a0d25f8606534f3d98
Yi <62215204+Yi-like-E@users.noreply.github.com>
2021-04-22 13:00:24 -0400

    Add files via upload

---
+++ Semester 2/WEB222/Assignments/5/src/js/app.js
@@ -0,0 +1,99 @@
+// An instance of our SimpleMap, created below when the window loads.
+let map;
+
+// Update the map to show markers for the set of observations
+function updateMap(observations, map) {
+  // Clear the current markers on the map (if any)
+  map.clear();
+
+  // TODO: call the Simple Map's addObservation method for every one
+  // of the observations in order to add markers to the map.
+  observations.forEach((observation) => {
+    map.addObservation(observation);
+  });
+}
+
+// Update the card to show markers for the set of observations
+function updateCard(observations) {
+  // Remove any current data from the card
+  clearAllCards();
+
+  // Populate the table with all observation data we want to show
+  observations.forEach((observation) => {
+    // TODO: call the buildRowForObservation function with the current observation
+    // and use that to add it to the table with the addRowToTable function.
+    buildCardForObservation(observation);
+    //addRowToTable(row);
+  });
+}
+
+// Show all species on the map and card
+function showAll() {
+  // Get all the observations from our data.js and format them so we can work with the data
+  const observations = getAllObservations();
+
+  // Update the map and table
+  updateMap(observations, map);
+  updateCard(observations);
+  updateCardTitle(`All Species (${observations.length})`);
+}
+
+// Show native species on the map and table
+function showOnlyNative() {
+  // Get all the observations from our data.js and format them so we can work with the data
+  const observations = getAllObservations();
+  // Filter out any that aren't native species
+  const native = filterOnlyNative(observations);
+
+  // Update the map and table
+  updateMap(native, map);
+  updateCard(native);
+  updateCardTitle(`Only Native Species (${native.length})`);
+}
+
+// Show introduced species on the map and table
+function showOnlyIntroduced() {
+  // Get all the observations from our data.js and format them so we can work with the data
+  const observations = getAllObservations();
+  // Filter out any that aren't introduced species
+  const introduced = filterOnlyIntroduced(observations);
+
+  // Update the map and table
+  updateMap(introduced, map);
+  updateCard(introduced);
+  updateCardTitle(`Only Introduced Species (${introduced.length})`);
+}
+
+function start() {
+  // Create our map object for Seneca's Newnham campus
+  map = new SimpleMap("map-container", 43.7955, -79.3496);
+
+  // TODO: create click handlers for all three buttons.
+  // The "All Species" button should call the showAll function.
+  // The "Only Native Species" button should call the showOnlyNative function.
+  // The "Only Introduced Species" button should call the showOnlyIntroduced function.
+  // In your solution, show both ways of creating click event handlers (i.e.,
+  // using the on* property and also addEventListener function).
+  let allSpecies = document.querySelector("#show-all");
+  let native = document.querySelector("#show-native");
+  let introduced = document.querySelector("#show-introduced");
+  allSpecies.addEventListener("click", function (e) {
+    showAll();
+  });
+  native.addEventListener("click", function (e) {
+    console.log("native button clicked");
+    showOnlyNative();
+  });
+  introduced.addEventListener("click", function (e) {
+    showOnlyIntroduced();
+  });
+  // Show all species observations by default when we start.
+  showAll();
+}
+
+// TODO: replace this console.log with the code necessary to call the start
+// function when the page has finished fully loading.
+window.addEventListener("load", function (e) {
+  //console.log(`OK, let's begin!`);
+  start();
+});
